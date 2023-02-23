13a75a2db5ba03485e5e109f4e2346d672be47b3
melinamorales512 <melinamorales956@gmail.com>
2023-02-22 21:44:24 -0600

    user-route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const thoughtRoutes = require('./thought-routes');
-const userRoutes = require('./user-routes');
+const userRoutes = require('./user-route');
 
 
 
